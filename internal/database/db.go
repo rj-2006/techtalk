5efcc75390d02d82743afe971c411cd50c0e1480
rj-2006 <keturahu295@gmail.com>
2026-01-28 22:38:56 +0530

    database setup

---
+++ internal/database/db.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/rj-2006/tech-talk/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect(host, user, password, dbname, port string) error {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disbale",
+		host, user, password, dbname, port)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to the database: %w", err)
+	}
+
+	log.Println("Database connected successfully!")
+
+	return nil
+}
+
+func Migrate() error {
+	return DB.AutoMigrate(
+		&models.User{},
+		&models.Thread{},
+		&models.Post{},
+		&models.Chatroom{},
+		&models.ChatMessage{},
+		&models.GameRoom{},
+		&models.GameState{},
+	)
+}
---
+++ internal/models/models.go
@@ -0,0 +1,71 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Username  string         `gorm:"unique;not null" json:"username"`
+	Email     string         `gorm:"unique;not null" json:"email"`
+	Password  string         `gorm:"not null" json:"-"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+type Thread struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Title     string    `gorm:"not null" json:"title"`
+	UserID    uint      `gorm:"not null" json:"user_id"`
+	User      User      `gorm:"foreignKey:UserID" json:"user"`
+	Posts     []Post    `gorm:"foreignKey:ThreadID" json:"posts,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Post struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Content   string    `gorm:"type:text;not null" json:"content"`
+	ThreadID  uint      `gorm:"not null" json:"thread_id"`
+	UserID    uint      `gorm:"not null" json:"user_id"`
+	User      User      `gorm:"foreignKey:UserID" json:"user"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Chatroom struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Name        string    `gorm:"not null" json:"name"`
+	Description string    `gorm:"type:text" json:"description"`
+	CreatedBy   uint      `gorm:"not null" json:"created_by"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type ChatMessage struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	ChatroomID uint      `gorm:"not null;index" json:"chatroom_id"`
+	UserID     uint      `gorm:"not null" json:"user_id"`
+	User       User      `gorm:"foreignKey:UserID" json:"user"`
+	Content    string    `gorm:"type:text;not null" json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+type GameRoom struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	Name       string    `gorm:"not null" json:"name"`
+	GameType   string    `gorm:"not null" json:"game_type"` // "smash_karts"
+	MaxPlayers int       `gorm:"default:8" json:"max_players"`
+	Status     string    `gorm:"default:'waiting'" json:"status"` // waiting, playing, finished
+	CreatedBy  uint      `gorm:"not null" json:"created_by"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+type GameState struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	GameRoomID uint      `gorm:"not null;uniqueIndex" json:"game_room_id"`
+	StateData  string    `gorm:"type:jsonb" json:"state_data"` // JSON blob
+	UpdatedAt  time.Time `json:"updated_at"`
+}
