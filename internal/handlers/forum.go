fe778eb0b61d54dd375b2db9e6b7914a9668f83d
rj-2006 <keturahu295@gmail.com>
2026-01-30 23:19:18 +0530

    creating posts

--- internal/handlers/forum.go
+++ internal/handlers/forum.go
@@ -52,3 +52,44 @@ func GetThread(c *gin.Context) {
 
 	c.JSON(http.StatusOK, thread)
 }
+
+func CreatePost(c *gin.Context) {
+	threadID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid thread id"})
+		return
+	}
+
+	var req struct {
+		Content string `json:"content" binding:"required,min=1"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var thread models.Thread
+
+	if err := database.DB.First(&thread, threadID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Thread not Found"})
+		return
+	}
+
+	userID := c.GetUint("user_id")
+
+	post := models.Post{
+		Content:  req.Content,
+		ThreadID: uint(threadID),
+		UserID:   userID,
+	}
+
+	if err := database.DB.Create(&post).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create Post."})
+		return
+	}
+
+	database.DB.Preload("User").First(&post, post.ID)
+
+	c.JSON(http.StatusCreated, post)
+}
