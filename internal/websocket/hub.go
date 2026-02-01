c1eeabaf58bfb6a364b5f09b61e8895841687f3a
rj-2006 <keturahu295@gmail.com>
2026-02-01 11:46:56 +0530

    hub

--- internal/websocket/hub.go
+++ internal/websocket/hub.go
@@ -1,1 +1,125 @@
 package websocket
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+)
+
+type BroadcastMessage struct {
+	RoomID  string
+	Message []byte
+	Sender  *Client
+}
+
+type Hub struct {
+	Rooms map[string]map[*Client]bool
+
+	Broadcast chan *BroadcastMessage
+
+	Register chan *Client
+
+	Unregister chan *Client
+
+	mu sync.RWMutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		Rooms:      make(map[string]map[*Client]bool),
+		Broadcast:  make(chan *BroadcastMessage),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.Register:
+			h.mu.Lock()
+			if h.Rooms[client.RoomID] == nil {
+				h.Rooms[client.RoomID] = make(map[*Client]bool)
+			}
+			h.Rooms[client.RoomID][client] = true
+			h.mu.Unlock()
+
+			log.Printf("Client %s joined room %s", client.Username, client.RoomID)
+
+			h.notifyJoin(client)
+
+		case client := <-h.Unregister:
+			h.mu.Lock()
+			if clients, ok := h.Rooms[client.RoomID]; ok {
+				if _, exists := clients[client]; exists {
+					delete(clients, client)
+					close(client.Send)
+
+					if len(clients) == 0 {
+						delete(h.Rooms, client.RoomID)
+					}
+				}
+			}
+			h.mu.Unlock()
+
+			log.Printf("Client %s left room %s", client.Username, client.RoomID)
+			h.notifyLeave(client)
+
+		case broadcastMsg := <-h.Broadcast:
+			h.mu.RLock()
+			clients := h.Rooms[broadcastMsg.RoomID]
+			h.mu.Unlock()
+
+			for client := range clients {
+				select {
+				case client.Send <- broadcastMsg.Message:
+				default:
+					h.mu.Lock()
+					delete(h.Rooms[broadcastMsg.RoomID], client)
+					close(client.Send)
+					h.mu.Unlock()
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) notifyJoin(client *Client) {
+	msg := Message{
+		Type:     MessageTypeJoin,
+		RoomID:   client.RoomID,
+		UserID:   client.UserID,
+		Username: client.Username,
+	}
+	h.broadcastToRoom(client.RoomID, msg)
+}
+
+func (h *Hub) notifyLeave(client *Client) {
+	msg := Message{
+		Type:     MessageTypeLeave,
+		RoomID:   client.RoomID,
+		UserID:   client.UserID,
+		Username: client.Username,
+	}
+	h.broadcastToRoom(client.RoomID, msg)
+}
+
+func (h *Hub) broadcastToRoom(roomID string, msg Message) {
+	data, _ := json.Marshal(msg)
+	h.mu.RLock()
+	clients := h.Rooms[roomID]
+	h.mu.Unlock()
+
+	for client := range clients {
+		select {
+		case client.Send <- data:
+		default:
+		}
+	}
+}
+
+func (h *Hub) GetRoomClients(roomID string) int {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	return len(h.Rooms[roomID])
+}
