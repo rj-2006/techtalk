3d4924d78d9aba78c1f8781de777b5dccb546115
rj-2006 <keturahu295@gmail.com>
2026-02-01 11:22:39 +0530

    client.go

--- internal/websocket/client.go
+++ internal/websocket/client.go
@@ -1,6 +1,7 @@
 package websocket
 
 import (
+	"log"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -20,4 +21,63 @@ type Client struct {
 	RoomID   string
 	UserID   uint
 	Username string
+}
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.Hub.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetReadLimit(maxMessageSize)
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Websocket error: %v", err)
+			}
+			break
+		}
+
+		c.Hub.Broadcast <- &BroadcastMessage{
+			RoomID:  c.RoomID,
+			Message: message,
+			Sender:  c,
+		}
+	}
+}
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			if err := c.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
