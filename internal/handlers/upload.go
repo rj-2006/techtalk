5b0af327780cad4e99e72749a1d7baa979f97405
rj-2006 <keturahu295@gmail.com>
2026-02-25 01:13:12 +0530

    upload handler

--- internal/handlers/upload.go
+++ internal/handlers/upload.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,4 +26,27 @@ func UploadAvatar(c *gin.Context) {
 	userID := c.GetUint("user_id")
 
 	file, err := c.FormFile("avatar")
+	if err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":"No File Uploaded"})
+		return 
+	}
+
+	if file.size > MaxAvatarSize {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file size is too big"})
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext != ".jpg" && ext! = ".jpeg" && ext != ".png" && ext != ".webp" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid file type"})
+		return
+	}
+
+	filename := fmt.Sprintf("%d_%s%s", userID, uuid.New().String(), ext)
+	filepath := filepath.Join(UploadPath, "avatars", filename)
+
+	if err := c.SaveUploadedFile(file,filepath); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save file"})
+		return
+	}
 }
